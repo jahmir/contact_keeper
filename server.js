2735ff2ff5d09568483d67675fdad586951897f1
jahmir <altairjohnfbacc@gmail.com>
2020-12-13 23:15:44 +0800

    many adds

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const connectDB = require('./config/db')
+
+//Connect database
+connectDB()
+
+//Init middleware
+app.use(express.json({extended: false}))
 
 app.get('/', (req, res) => res.json({msg: 'Welcom to the contactkeeper API'}))
 
