af32e6776a2c041717d4f123196f05b42f59b5e5
jahmir <altairjohnfbacc@gmail.com>
2020-12-14 22:21:23 +0800

    updates

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,10 @@ const express = require('express')
 const router = express.Router();
 const bcrypt = require('bcryptjs')
 const {check, validationResult} =require('express-validator/check')
-
+const jwt = require('jsonwebtoken')
+const config = require('config')
 const User = require('../models/User')
+
 
 //@route POST api/users
 //@desc Register a user
@@ -39,8 +41,18 @@ router.post('/', [
         user.password = await bcrypt.hash(password, salt)
 
         await user.save()
-        res.send('User saved')
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
 
+        jwt.sign(payload, config.get('jwtSecret'), {
+            expiresIn: 36000
+        }, (err, token) => {
+            if(err) throw err;
+            res.json({token})
+        })
     } catch (error) {
         console.log(error.message);
         res.status(500).send('Server error')
