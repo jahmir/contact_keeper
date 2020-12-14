bb23c1cf77f70695855844de96f0e8526fac1e58
jahmir <altairjohnfbacc@gmail.com>
2020-12-14 13:07:29 +0800

    updated

--- routes/users.js
+++ routes/users.js
@@ -13,13 +13,21 @@ router.post('/', [
     check('password', 'Please enter a password with 6 or more characters').isLength({
         min: 6
     })
-],(req, res) => {
+],async (req, res) => {
     const errors = validationResult(req)
     if(!errors.isEmpty()){
         return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('passed')
+    const { name, email, password } = req.body;
+
+    try {
+        let user = await User.findOne({email})
+    } catch (error) {
+        
+    }
+
+
 });
 
 module.exports = router;
